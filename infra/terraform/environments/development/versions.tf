751aef2186189106371c6ff9d53731c2e7a31d97
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2025-03-07 20:17:13 -0500

    Add docker provider

--- infra/terraform/environments/development/versions.tf
+++ infra/terraform/environments/development/versions.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
+        docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
     google = {
       source  = "hashicorp/google"
       version = "6.24.0"
     }
+    
   }
 }
--- infra/terraform/modules/deploy/versions.tf
+++ infra/terraform/modules/deploy/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+        docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
     google = {
       source  = "hashicorp/google"
       version = "6.24.0"
