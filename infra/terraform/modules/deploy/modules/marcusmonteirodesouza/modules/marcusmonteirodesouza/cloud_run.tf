716cfba5a3730845be00b33b3dc55f4c1a719c80
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2025-03-07 22:31:08 -0500

    Allow unauthenticated invocations, set max instance count to 1

--- infra/terraform/modules/deploy/modules/marcusmonteirodesouza/modules/marcusmonteirodesouza/cloud_run.tf
+++ infra/terraform/modules/deploy/modules/marcusmonteirodesouza/modules/marcusmonteirodesouza/cloud_run.tf
@@ -18,6 +18,11 @@ resource "google_cloud_run_v2_service" "marcusmonteirodesouza" {
   template {
     service_account = var.service_account_email
 
+    scaling {
+      # TODO(Marcus): scale to more when needed
+      max_instance_count = 1
+    }
+
     containers {
       image = var.image
 
@@ -40,4 +45,12 @@ resource "google_cloud_run_v2_service" "marcusmonteirodesouza" {
       egress = "ALL_TRAFFIC"
     }
   }
+}
+
+resource "google_cloud_run_service_iam_member" "marcusmonteirodesouza_allow_unauthenticated" {
+  location = google_cloud_run_v2_service.marcusmonteirodesouza.location
+  project  = google_cloud_run_v2_service.marcusmonteirodesouza.project
+  service  = google_cloud_run_v2_service.marcusmonteirodesouza.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
